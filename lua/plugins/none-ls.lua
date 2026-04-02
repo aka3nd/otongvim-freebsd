c9155754304404729c5d7eed31a7d8029067f3b5
Aka3nd <fileaing1@gmail.com>
2026-04-02 22:40:26 +0700

    add none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,14 +8,21 @@ return {
 			null_ls.setup({
 				sources = {
 					--formatter
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.clang_format,
+					null_ls.builtins.formatting.stylua.with({
+						filetypes = { "lua", "luau" },
+						command = "stylua",
+					}),
+
+					null_ls.builtins.formatting.clang_format.with({
+						filetypes = { "c", "cpp", "cs", "java", "cuda", "proto" },
+						command = "clang-format19",
+					}),
 
 					--linter
-					--null_ls.builtins.diagnostics.cppcheck.with({
-						--filetypes = { "c", "cpp", "objc", "objcpp" },
-						--command = "/usr/local/bin/cppcheck",
-					--}),
+					null_ls.builtins.diagnostics.cppcheck.with({
+						filetypes = { "c", "cpp", "objc", "objcpp" },
+						command = "/usr/local/bin/cppcheck",
+					}),
 				},
 			})
 
